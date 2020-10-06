d369d1543de392d98cf9b5751b4979bec182ed91
Darlan de Moraes Silveira <ntconsult.darlan@agibank.com.br>
2020-10-06 16:14:41 -0300

    new code

--- index.js
+++ index.js
@@ -1,17 +1,42 @@
-const express = require('express');
-var proxy = require('express-http-proxy');
+const express = require("express");
+const proxy = require("express-http-proxy");
 const app = express();
 const port = 8080;
+const AWS = require("aws-sdk");
+AWS.config.update({
+  region: process.env.REGION
+});
+const elbv2 = new AWS.ELBv2();
 
-app.get('/', (req, res) => {
-  res.send('Hello World!');
+const DNSPrefix = "internal-tenant-";
+const LoadBalancers = {};
+
+setInterval(async () => {
+  try {
+    console.log("Loading LoadBalancers...");
+    const response = await elbv2.describeLoadBalancers({}).promise();
+    for (const LoadBalancer of response.LoadBalancers) {
+      const { DNSName } = LoadBalancer;
+      if (DNSName.startsWith(DNSPrefix)) {
+        const DNSKey = DNSName.replace(DNSPrefix, "").split("-")[0];
+        Object.assign(LoadBalancers, { [DNSKey]: DNSName })
+      }
+    }
+    console.log(`Loaded LoadBalancers... ${JSON.stringify(LoadBalancers)}`);
+  } catch(e) {
+    console.error("Failure Loading LoadBalancers...", e);
+  }
+}, 10 * 1000);
+
+function getLoadBalancer(req) {
+  return LoadBalancers[req.query.app];
+}
+
+app.get("/", (req, res) => {
+  res.send("{\"status\": \"ok\"}");
 })
 
-app.get('/test', (req, res) => {
-  res.send('Test!');
-})
-
-app.use('/proxy', proxy('http://internal-tf-lb-20201005221956071000000001-1260404789.sa-east-1.elb.amazonaws.com'));
+app.use("/proxy", proxy(getLoadBalancer));
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`);
