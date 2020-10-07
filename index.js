90c2f503260275bec93d169938562417ab539ac6
Darlan de Moraes Silveira <ntconsult.darlan@agibank.com.br>
2020-10-07 14:50:32 -0300

    new version

--- index.js
+++ index.js
@@ -8,35 +8,52 @@ AWS.config.update({
 });
 const elbv2 = new AWS.ELBv2();
 
-const DNSPrefix = "internal-tenant-";
-const LoadBalancers = {};
+const DNSPrefix = "internal-";
+const DNSSuffix = "LoadBalancer";
+const LoadBalancers = [];
 
 setInterval(async () => {
   try {
-    console.log("Loading LoadBalancers...");
+    LoadBalancers.splice(0, LoadBalancers.length);
     const response = await elbv2.describeLoadBalancers({}).promise();
     for (const LoadBalancer of response.LoadBalancers) {
       const { DNSName } = LoadBalancer;
       if (DNSName.startsWith(DNSPrefix)) {
-        const DNSKey = DNSName.replace(DNSPrefix, "").split("-")[0];
-        Object.assign(LoadBalancers, { [DNSKey]: DNSName })
+        const DNSKey = DNSName.replace(DNSPrefix, "").split(DNSSuffix)[0];
+        LoadBalancers.push({
+          client: DNSKey,
+          path: `/backend-for-frontend/app/api/execute?client=${DNSKey}`,
+          internal: `http://${DNSName}`
+        });
       }
     }
-    console.log(`Loaded LoadBalancers... ${JSON.stringify(LoadBalancers)}`);
   } catch(e) {
-    console.error("Failure Loading LoadBalancers...", e);
+    console.error("Can't find LoadBalancers...", e);
   }
 }, 10 * 1000);
 
-function getLoadBalancer(req) {
-  return LoadBalancers[req.query.app];
+function getMatch(req) {
+  const client = req.query.client;
+  const match = LoadBalancers.filter(LoadBalancer => LoadBalancer.client === client);
+  if (match.length) {
+    return match[0].internal;
+  }
 }
 
 app.get("/", (req, res) => {
-  res.send("{\"status\": \"ok\"}");
+  res.send({ camunda: LoadBalancers });
 })
 
-app.use("/proxy", proxy(getLoadBalancer));
+app.use("/backend-for-frontend", proxy(getMatch, {
+  filter: (req, res) => {
+    const match = !!getMatch(req);
+    if (!match) {
+      res.status(404)
+         .send({ status: "Not Found" });
+    }
+    return match;
+  }
+}));
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`);
